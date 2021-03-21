f7acdd15305dad317c7802e024368c20f92affeb
suarez9093 <suarez9093@hotmail.com>
2021-03-21 15:28:36 -0400

    query working. Building out the search

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
+  useEffect(() => {
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=dogs&key=AIzaSyB3_qVj6qvToTWoVpoXelHGcayvtTJvcHs'
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  });
   return (
     <div className='app'>
       <Header />
