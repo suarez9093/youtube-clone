e8eab27c155b1d13c84369294cf8e3f1a9999c89
suarez9093 <suarez9093@hotmail.com>
2021-03-25 10:26:29 -0400

    added readme

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyB3_qVj6qvToTWoVpoXelHGcayvtTJvcHs',
+  authDomain: 'clone-308318.firebaseapp.com',
+  projectId: 'youtube-clone-308318',
+  storageBucket: 'youtube-clone-308318.appspot.com',
+  messagingSenderId: '90016030538',
+  appId: '1:90016030538:web:2fa2e0b28219a4a03a6bb3',
+  measurementId: 'G-EWC4RZ06VE',
+};
+
+firebase.initializeApp(firebaseConfig);
